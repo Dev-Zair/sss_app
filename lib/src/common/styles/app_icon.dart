f6579d4404432116e5b16aac364a3dfb0cc2e986
Dev-Zair <zairhan2111@gmail.com>
2025-09-06 02:36:02 +0500

    Добавил icon

---
+++ lib/src/common/styles/app_icon.dart
@@ -0,0 +1,21 @@
+final class AppIcons {
+  static const String  home = 'assets/icons/home.svg';
+  static const String  user = 'assets/icons/user.svg';
+  static const String  settings = 'assets/icons/settings.svg';
+  static const String  chat = 'assets/icons/chat.svg';
+  static const String  notification = 'assets/icons/notification.svg';
+  static const String  security = 'assets/icons/security.svg';
+  static const String  help = 'assets/icons/help.svg';
+  static const String  scanner = 'assets/icons/scanner.svg';
+  static const String  revision = 'assets/icons/revision.svg';
+  static const String  receiving = 'assets/icons/receiving.svg';
+  static const String  report = 'assets/icons/report.svg';
+  static const String  product = 'assets/icons/product.svg';
+  static const String  movement = 'assets/icons/movement.svg';
+  static const String  language = 'assets/icons/language.svg';
+  static const String  kassa = 'assets/icons/kassa.svg';
+  static const String  add = 'assets/icons/add.svg';
+  static const String  headphones = 'assets/icons/headphones.svg';
+  static const String  search = 'assets/icons/search.svg';
+  static const String  category = 'assets/icons/category.svg';
+}
